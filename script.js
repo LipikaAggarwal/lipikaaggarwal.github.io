5f67a0c9c0a4dbb7ce68ea07b7ec03620472c335
Lipika Aggarwal <2212lipika@gmail.com>
2024-10-30 00:40:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const dynamicText = document.querySelector('.dynamic-text');
+const cursor = document.querySelector('.cursor');
+
+// Array of phrases to cycle through
+const phrases = [
+    "a B.Tech student",
+    "a tech enthusiast",
+    "a developer",
+    "an AI/ML enthusiast",
+    "passionate about innovation",
+    "skilled in Python and C",
+    "proficient in HTML, CSS, and JavaScript",
+    "driven to solve real-world problems",
+    "a quick learner",
+    "collaborative and team-oriented",
+    "a problem solver"
+];
+
+let currentPhraseIndex = 0;
+let currentLetterIndex = 0;
+let currentPhrase = "";
+const typingSpeed = 100; 
+const delayBetweenPhrases = 2000; 
+
+
+function typeLetter() {
+    if (currentLetterIndex < currentPhrase.length) {
+        dynamicText.textContent += currentPhrase[currentLetterIndex];
+        currentLetterIndex++;
+        setTimeout(typeLetter, typingSpeed);
+    } else {
+        
+        setTimeout(clearText, delayBetweenPhrases);
+    }
+}
+
+
+function clearText() {
+    dynamicText.textContent = "";
+    currentLetterIndex = 0;
+    currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length; 
+    currentPhrase = phrases[currentPhraseIndex];
+    setTimeout(typeLetter, typingSpeed); 
+}
+typeLetter();
+
+const gallery = document.querySelector('.event-gallery');
+
+gallery.addEventListener('mouseover', () => {
+    gallery.style.animationPlayState = 'paused'; 
+});
+
+gallery.addEventListener('mouseout', () => {
+    gallery.style.animationPlayState = 'running'; 
+});
