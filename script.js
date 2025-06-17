ca4d3e0dd5f8fc941cb538793fc12562762e7353
Lipika Aggarwal <2212lipika@gmail.com>
2025-06-18 00:35:28 +0530

    Updated Contact Form

--- script.js
+++ script.js
@@ -65,3 +65,18 @@ menuLinks.forEach((link) => {
     });
 });
 
+ const form = document.getElementById("contactme");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+
+      const subject = encodeURIComponent("Portfolio Contact from " + name);
+      const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`);
+
+      const mailtoLink = `mailto:lipikaaggarwal@yahoo.com?subject=${subject}&body=${body}`;
+      window.location.href = mailtoLink;
+    });
